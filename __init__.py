5bec546065f125b83722ce04f21ba5e82111e4a9
Kerrick Staley <kerrick@kerrickstaley.com>
2017-12-25 12:53:37 -0800

    Create ChinesePrestudy class

--- __init__.py
+++ __init__.py
@@ -5,28 +5,37 @@ from aqt.qt import *
 from PyQt5 import QtCore
 
 
-def show_text_entry_window():
+class ChinesePrestudy:
     """
-    Show the first window of the utility. This window prompts the user to paste in some text.
+    Class that manages all the state associated with the Chinese Prestudy add-on.
     """
-    w = QWidget(mw, flags=QtCore.Qt.Window)
-    w.setWindowTitle('Chinese Prestudy')
 
-    vbox = QVBoxLayout()
+    @classmethod
+    def instantiate_and_run(cls):
+        cls().show_text_entry_window()
 
-    vbox.addWidget(QLabel('Paste in the Chinese text you want to read:'))
+    def show_text_entry_window(self):
+        """
+        Show the first window of the utility. This window prompts the user to paste in some text.
+        """
+        w = QWidget(mw, flags=QtCore.Qt.Window)
+        w.setWindowTitle('Chinese Prestudy')
 
-    text_box = QTextEdit()
-    vbox.addWidget(text_box)
+        vbox = QVBoxLayout()
 
-    w.setLayout(vbox)
+        vbox.addWidget(QLabel('Paste in the Chinese text you want to read:'))
 
-    w.show()
+        text_box = QTextEdit()
+        vbox.addWidget(text_box)
+
+        w.setLayout(vbox)
+
+        w.show()
 
 
 # create a new menu item, "test"
 action = QAction("test", mw)
 # set it to call testFunction when it's clicked
-action.triggered.connect(show_text_entry_window)
+action.triggered.connect(ChinesePrestudy.instantiate_and_run)
 # and add it to the tools menu
 mw.form.menuTools.addAction(action)
