abf177b0cf9ae8b400807c78757f904c120c0102
Kerrick Staley <kerrick@kerrickstaley.com>
2018-06-30 13:06:15 -0700

    Install genanki from local

--- package.py
+++ package.py
@@ -15,7 +15,6 @@ SOURCE_FILES = [
 DEPENDENCIES_PYPI = [
   'jieba',
   'cached_property',
-  'genanki',
 ]
 
 DEPENDENCIES_LOCAL = [
@@ -23,6 +22,7 @@ DEPENDENCIES_LOCAL = [
   'chineseflashcards',
   'pystache',
   'yaml',
+  'genanki',
 ]
 
 PACKAGE_CACHE_DIR = 'package_cache'
