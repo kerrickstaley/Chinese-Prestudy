c1f4721c87e9d574c8a5721213490265e0d3e66c
Kerrick Staley <kerrick@kerrickstaley.com>
2018-08-07 18:24:06 -0700

    Add test on unknown_words

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,4 +1,4 @@
-from mock import MagicMock
+import mock
 import types
 import sys
 
@@ -22,7 +22,7 @@ def mock_imports():
 
   for o in objects:
     mod, name = o.rsplit('.', 1)
-    setattr(sys.modules[mod], name, MagicMock())
+    setattr(sys.modules[mod], name, mock.MagicMock())
 
 mock_imports()
 
@@ -34,3 +34,11 @@ def test_input_words():
   cp.input_text = '你好！我叫李可達。我喜歡攀岩。'
 
   assert cp.input_words == ['你好', '我', '叫', '李可達', '喜歡', '攀岩']
+
+
+def test_unknown_words():
+  cp = ChinesePrestudy()
+  cp.input_text = '你好！我叫李可達。我喜歡攀岩。'
+
+  with mock.patch.object(cp, 'words_already_studied', new={'你好', '我', '喜歡'}):
+    assert cp.unknown_words == ['叫', '李可達', '攀岩']
